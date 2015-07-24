8c825a096279124ac3418a806605f9169c0928fa
wesckleyu <wesckleyu@ZeroCoEnterprise>
2015-07-23 21:31:54 -0300

    Update Paginator.php example

--- examples/Paginator.php
+++ examples/Paginator.php
@@ -1,44 +1,46 @@
 <?php
 
-namespace Application\Layers\Controllers;
+namespace Application\Controllers;
 
-use Application\Layers\Models;
+use Application\Models;
 use Hideks\Controller;
 use Hideks\Paginator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends Controller {
-    
-    public function indexAction(Request $request, $page = 1) {
+class UserController extends Controller
+{
+
+    public function indexAction(Request $request, $page = 1)
+    {
         $paginator = new Paginator(array(
-            'totalItens'    => Models\User::count(),
-            'currentPage'   => $page,
-            'limitPerPage'  => 5
+            'totalItens'   => Models\User::count(),
+            'currentPage'  => $page,
+            'limitPerPage' => 5
         ));
-        
+
         /* ################################################################## *
          * #### Create a custom pagination with the getAttributes method #### *
          * ################################################################## */
-        
+
         var_export($paginator->getAttributes());
-        
-        
-        
+
+
+
         /* ################################################################## *
          * ########################### Desktop ############################## *
          * ################################################################## */
-        
+
         // Basic desktop pagination
         $pagination = $paginator->desktop('route_pagination');
-        
+
         // Desktop pagination with aditional params
         $pagination = $paginator->desktop('other_route_pagination', array(
             'params' => array(
                 'name' => 'value'
             )
         ));
-        
+
         // Desktop pagination with alternative route name
         $pagination = $paginator->desktop(array(
             'routes' => array(
@@ -46,35 +48,35 @@ class UserController extends Controller {
                 'other_route_pagination'
             )
         ));
-        
+
         // Desktop pagination with all options
         $pagination = $paginator->desktop(array(
             'routes' => array(
                 'route',
                 'other_route_pagination'
             ),
-        ), array(
+                ), array(
             'params' => array(
                 'name' => 'value'
             )
         ));
-        
-        
-        
+
+
+
         /* ################################################################## *
          * ############################# Mobile ############################# *
          * ################################################################## */
-        
+
         // Basic mobile pagination
         $pagination = $paginator->mobile('route_pagination');
-        
+
         // Mobile pagination with aditional params
         $pagination = $paginator->mobile('other_route_pagination', array(
             'params' => array(
                 'name' => 'value'
             )
         ));
-        
+
         // Mobile pagination with alternative route name
         $pagination = $paginator->mobile(array(
             'routes' => array(
@@ -82,40 +84,41 @@ class UserController extends Controller {
                 'other_route_pagination'
             )
         ));
-        
+
         // Mobile pagination with all options
         $pagination = $paginator->mobile(array(
             'routes' => array(
                 'route',
                 'other_route_pagination'
             )
-        ), array(
+                ), array(
             'params' => array(
                 'name' => 'value'
             )
         ));
-        
-        
-        
+
+
+
         /* ################################################################### *
          * ######################### Data pagination ######################### *
          * ################################################################### */
-        
+
         $options = array(
-            'limit'     => $paginator->getLimit(),
-            'offset'    => $paginator->getOffset()
+            'limit'  => $paginator->getLimit(),
+            'offset' => $paginator->getOffset()
         );
-        
+
         $users = array();
 
         foreach (Models\User::all($options) as $user) {
             $users[] = $user;
         }
         
-        return $this->renderTo('users/index', array(
-            'users'      => $users,
-            'pagination' => $pagination
-        ), new Response());
+        $this->view->users = $users;
+        
+        $this->view->pagination = $pagination;
+
+        return $this->renderTo('users/index.html', new Response());
     }
-    
+
 }
